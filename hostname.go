8e1d6b74cda48bb9c1365c31cac9fc302b9d13a3
Marc Sluiter <marc.sluiter@luminis.eu>
2017-08-02 16:36:30 +0200

    make delay configurable via DELAY system environment variable

--- hostname.go
+++ hostname.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 var hostname string
 
 func main() {
-	fmt.Println("Started, but going to sleep...")
+	delay := os.Getenv("DELAY")
+	delay_s, err := strconv.Atoi(delay)
+	if err != nil {
+		fmt.Println("couldn't read delay time from env")
+		delay_s = 20
+	}
+	fmt.Printf("Started, but going to sleep for %v seconds\n", delay_s)
 	hostname, _ = os.Hostname()
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * time.Duration(delay_s))
 	http.HandleFunc("/", handler)
 	fmt.Println("And awake, ready for business!")
 	http.ListenAndServe(":8080", nil)
